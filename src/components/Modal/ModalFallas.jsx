75e17d9b8be37e0e5cf2d343f4220776a897ff17
Keiber linarez <keibermiguel98@gmail.com>
2024-04-05 11:04:11 -0400

    km-dev - formulario integrado en modal, tabla recortada 50% para la ilustracion de datos

--- src/components/Modal/ModalFallas.jsx
+++ src/components/Modal/ModalFallas.jsx
@@ -1,4 +1,4 @@
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Row, Col, FormGroup, Input } from 'reactstrap';
 import React, { useState } from 'react';
 
 export const ModalFallas=(args)=>{
@@ -8,18 +8,71 @@ export const ModalFallas=(args)=>{
     return(
         <>
         <Button className="my-4" color="success" type="button" onClick={toggle}>
-         <i className="ni ni-fat-add ni-md text-white"/>
-             Añadir
+             Anexar nueva falla
         </Button>
 
         <Modal isOpen={modal} toggle={toggle} {...args}>
-          <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+          <ModalHeader toggle={toggle}>NUEVA FALLA</ModalHeader>
          <ModalBody>
-            Formulario add
+         <div className="pl-lg-4">
+                    <Row>
+                   
+                      <Col lg="12">
+                        <FormGroup>
+                          <label
+                            className="form-control-label"
+                            htmlFor="input-email"
+                          >
+                            Descripcion
+                          </label>
+                          <Input
+                            className="form-control-alternative"
+                            id="input-descripcion"
+                            placeholder="Example: Acetaminofen 200ML"
+                            type="text"
+                          />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col lg="6">
+                        <FormGroup>
+                          <label
+                            className="form-control-label"
+                            htmlFor="input-first-name"
+                          >
+                            Existencia
+                          </label>
+                          <Input
+                            className="form-control-alternative"
+                            id="input-existencia"
+                            placeholder="0"
+                            type="number"
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col lg="6">
+                        <FormGroup>
+                          <label
+                            className="form-control-label"
+                            htmlFor="input-last-name"
+                          >
+                            Cantidad sugerida
+                          </label>
+                          <Input
+                            className="form-control-alternative"
+                            id="input-sugerencia"
+                            placeholder="0"
+                            type="number"
+                          />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                  </div>
         </ModalBody>
         <ModalFooter>
           <Button color="primary" onClick={toggle}>
-            Do Something
+            Cargar falla
           </Button>{''}
           <Button color="secondary" onClick={toggle}>
             Cancel
