b172ace72fd236140f5b42dbeec3d51cfd9dd56d
Keiber linarez <keibermiguel98@gmail.com>
2024-04-05 11:48:41 -0400

    km-dev- Preparacion de formulario usuarios para almacenar datos y roles

--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,12 @@
-/*!
 
-=========================================================
-* Argon Dashboard React - v1.2.4
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
-* Copyright 2024 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import Index from "views/Index.js";
-import Profile from "views/examples/Profile.js";
 import Maps from "views/examples/Maps.js";
 import Register from "views/examples/Register.js";
 import Login from "views/examples/Login.js";
 import Tables from "views/examples/Tables.js";
 import Icons from "views/examples/Icons.js";
 import AddFallas from "views/examples/AddFallas";
+import Usuarios from "views/examples/Usuarios";
 
 var routes = [
   {
@@ -54,10 +38,10 @@ var routes = [
     layout: "/admin",
   },
   {
-    path: "/user-profile",
-    name: "User Profile",
+    path: "/users",
+    name: "Usuarios",
     icon: "ni ni-single-02 text-yellow",
-    component: <Profile />,
+    component: <Usuarios />,
     layout: "/admin",
   },
   {
