3d49888d503c31900c48ee6f906aa400b6b534d4
Keiber linarez <keibermiguel98@gmail.com>
2024-04-25 16:21:55 -0400

    login

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,7 +1,8 @@
 import { Navigate } from "react-router-dom"
 
 export const ProtectedRoute = ({user,children})=>{
-   if(!user){
+   if(user === ""){
+    console.log('no se encuentra user')
     return <Navigate to="/auth"/>
    }
   return children
