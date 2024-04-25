04590a00dabbb69e53771f23e13492282701b7df
Keiber linarez <keibermiguel98@gmail.com>
2024-04-25 16:07:56 -0400

    km-dev - Protegiendo routas

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom"
 
 export const ProtectedRoute = ({user,children})=>{
    if(!user){
-     <Navigate to="/auth"/>
+    return <Navigate to="/auth"/>
    }
   return children
 }
